ec50817ac7887233f3418f0bb9ae5950d7e30e38
i-josh <jihiabe@gmail.com>
2023-02-15 11:15:03 +0100

    changes

--- lib/src/index.dart
+++ lib/src/index.dart
@@ -2,9 +2,12 @@ import 'package:dio/dio.dart';
 import 'package:moneta_core_banking/src/repo/banking_repository.dart';
 import 'package:moneta_core_banking/src/utils/api_response.dart';
 
+import 'repo/banking_mock_repository.dart';
+import 'repo/banking_repo_interface.dart';
+
 class MonetaCoreBanking {
   final String requestToken;
-  late BankingRepository _bankingRepo;
+  late IBankingRepository _bankingRepo;
 
   MonetaCoreBanking({
     required this.requestToken,
---
+++ lib/src/repo/banking_mock_repository.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:moneta_core_banking/src/repo/banking_repo_interface.dart';
+import 'package:moneta_core_banking/src/utils/api_response.dart';
+
+class BankingRepositoryMock extends IBankingRepository {
+  final String token;
+
+  BankingRepositoryMock(this.token);
+
+  @override
+  Future<ApiResponse> doTransfer(
+      Map<String, dynamic> request, String id) async {
+    await Future.delayed(const Duration(seconds: 3));
+    return ApiResponse(Response(
+        statusCode: 200,
+        data: {
+          "status": "success",
+          "data": {},
+        },
+        requestOptions: RequestOptions(path: "")));
+  }
+
+  @override
+  Future<ApiResponse> doTransferOther(Map<String, dynamic> request, String id) {
+    // TODO: implement doTransferOther
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<ApiResponse> getBalance(String id) {
+    // TODO: implement getBalance
+    throw UnimplementedError();
+  }
+}
